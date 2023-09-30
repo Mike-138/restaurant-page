6f58dff43d323fafaf4d99df0ecab4831cef4281
Michael <mikemcd.bbs@gmail.com>
2023-09-30 13:22:16 -0400

    Implement display controller

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const component = () => {
-    const element = document.createElement("div");
-    element.innerHTML = "Hello webpack";
-    return element;
-}
+const displayController = (() => {
 
-const contentContainer = (() => {
-    const container = document.createElement("div");
-    container.id = "content";
-    return container;
+    const _component = () => document.createElement("div");
+    
+    const contentContainer = (() => {
+        const container = _component();
+        container.id = "content";
+        return container;
+    })();
+
 })();
 
 document.body.appendChild(contentContainer);
