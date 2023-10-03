58af3c95b0a8c3fce0ceef98d87993f6dcaaca64
Michael <mikemcd.bbs@gmail.com>
2023-10-03 13:47:06 -0400

    Implement basic menu functionality

--- src/index.js
+++ src/index.js
@@ -121,7 +121,83 @@ const landingPage = (() => {
 
 const menuPage = (() => {
 
-    return;
+    const _menuList = [];
+
+    const _makeMenuItem = (name, price, image, description) => {
+
+        const itemName = base.makeHeader();
+        itemName.textContent = name;
+
+        const itemPrice = base.makeHeader();
+        itemPrice.textContent = price;
+
+        const itemImage = new Image();
+        itemImage.src = image;
+
+        const itemDescription = base.makePara();
+        itemDescription.textContent = description;
+
+        const container = base.makeDiv();
+        container.classList.add("menu-item");
+        container.append(itemName, itemPrice, itemImage, itemDescription);
+        return container;
+    }
+
+    const _publishMenuItem = (name, price, image, description) => {
+        const menuItem = _makeMenuItem(name, price, image, description);
+        _menuList.push(menuItem);
+        return menuItem;
+    }
+
+    // Icon is a placeholder for images
+
+    const _hamburger = _publishMenuItem(
+        "Classic Hamburger",
+        8.99,
+        Icon,
+        "This classic homestyle hamburger is sure to quell whatever cravings you may have!"
+    );
+
+    const _hotDog = _publishMenuItem(
+        "Jumbo Glizzy",
+        5.99,
+        Icon,
+        "You can never go wrong with one of our flame-grilled glizzies!"
+    );
+
+    const _macAndCheese = _publishMenuItem(
+        "Mac 'n' Cheese",
+        7.99,
+        Icon,
+        "For the more sophisticated among you."
+    );
+
+    const _frenchFries = _publishMenuItem(
+        "French Fries",
+        2.99,
+        Icon,
+        "No fast food feast is complete without a few of these!"
+    );
+
+    const _softDrink = _publishMenuItem(
+        "Pop",
+        1.99,
+        Icon,
+        "To quench your thirst."
+    );
+
+    const _menuContainer = (() => {
+        const container = base.makeDiv();
+        container.classList.add("menu-container");
+        container.append(..._menuList);
+        return container;
+    })();
+
+    const render = () => {
+        base.contentContainer.replaceChildren(_menuContainer);
+    }
+
+    return { render };
 
 })();
 
@@ -205,6 +281,8 @@ const displayController = (() => {
     }
 
     navBar.navIcon.addEventListener("click", landingPage.render);
+
+    navBar.menuTab.addEventListener("click", menuPage.render);
 
     navBar.contactTab.addEventListener("click", contactPage.render);
 
